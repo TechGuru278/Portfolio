4ee8dd993a85792105e3e1a8666f250d090f6d87
Shreyas-Shridhar-Kulkarni <allok512566@gmail.com>
2024-10-08 00:05:59 +0530

    first commit

---
+++ mobile/script.js
@@ -0,0 +1,98 @@
+function menubarfuction() {
+    let menulist = document.querySelector(".menulist")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    let home = document.querySelector(".home")
+    // menulist.style.transition = "1s";
+    menulist.style.visibility = "visible";
+    exitimage.style.visibility = "Visible";
+    barline.style.visibility = "hidden";
+    // home.style.visibility = "hidden";
+    home.style.filter = "blur(10px)";
+    document.body.style.overflow = "hidden";
+
+}
+function exit() {
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+}
+
+function about(){
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+    window.scrollTo(0,690);
+}
+function resume(){
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+    window.scrollTo(0,1220);
+}
+function skills(){
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+    window.scrollTo(0,1790);
+}
+function project(){
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+    window.scrollTo(0,2090);
+}
+function contact(){
+    let menulist = document.querySelector(".menulist")
+    let home = document.querySelector(".home")
+    let exitimage = document.querySelector(".exit")
+    let barline = document.querySelector(".navimg")
+    // menulist.style.transition = "0.3s ease-out";
+    menulist.style.visibility = "hidden";
+    exitimage.style.visibility = "hidden";
+    barline.style.visibility = "visible";
+    home.style.visibility = "visile";
+    home.style.filter = "blur(0)";
+    document.body.style.overflow = "visible";
+    window.scrollTo(0,2560);
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+function homeform(){
+    window.scrollTo(0,1)
+}
+function aboutform(){
+    window.scrollTo(0,800)
+}
+function skillsform(){
+    window.scrollTo(0,1650)
+}
+function projectform(){
+    window.scrollTo(0,2650)
+}
+function contactform(){
+    window.scrollTo(0,10000)
+}
